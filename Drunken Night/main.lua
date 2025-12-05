f4ffd1d67f805b1126657c0dc87555272b7783a2
Taher1996 <elysianroad@hotmail.com>
2025-12-05 19:15:42 +0200

    Delete Drunken Night directory

--- Drunken Night/main.lua
+++
@@ -1,8 +0,0 @@
--- Hide status bar
-display.setStatusBar(display.HiddenStatusBar)
-
--- Load Composer scene manager
-local composer = require("composer")
-
--- Start first scene (menu)
-composer.gotoScene("scenes.scene_menu")
